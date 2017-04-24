3e2636dc110abf73b0d0701fd896aff7e9882d35
kaushalkishre007 <kaushalkishre007@gmail.com>
2017-04-24 13:05:23 +0530

    first commit
    test

---
+++ test_123/src/com/test/contollers/Login.java
@@ -0,0 +1,40 @@
+package com.test.contollers;
+
+
+
+
+import org.springframework.http.HttpStatus;
+import javax.ws.rs.core.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.models.User;
+
+@RestController
+public class Login {
+	@RequestMapping(value="/login",method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON)
+	public String login(@RequestBody User u1){
+		System.out.println("post is hit");
+		
+		if(u1.getUname()=="root" && u1.getPassword()=="root"){
+			return "ok";
+			
+		}else{
+			return null;
+		}		
+	}
+	@RequestMapping(value="/login",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON)
+	@ResponseStatus
+	public User login(){
+		System.out.println("i  am hit");
+		User u1=new User();
+		u1.setId(1);
+		u1.setUname("root");
+		u1.setPassword("root");
+		return u1; 
+		
+	}
+}
---
+++ test_123/src/com/test/models/User.java
@@ -0,0 +1,30 @@
+package com.test.models;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+	private int id;
+	private String uname;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
